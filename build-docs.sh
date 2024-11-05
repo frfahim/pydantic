574ab1d54cd8b6fa27d9e37397c04918d72d2ca3
Farhadur Reza Fahim <farhadurfahim@gmail.com>
2024-11-06 01:51:03 +0600

    Check python version with sync and cloudflare

--- build-docs.sh
+++ build-docs.sh
@@ -10,4 +10,13 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 ${HOME}/.cargo/bin/uv python install 3.12
 ${HOME}/.cargo/bin/uv sync --group docs --frozen
 ${HOME}/.cargo/bin/uv run python -c 'import docs.plugins.main'
+
+# Adding local symlinks gets nice source locations like
+#   pydantic_core/core_schema.py
+# instead of
+#   .venv/lib/python3.10/site-packages/pydantic_core/core_schema.py
+ln -s .venv/lib/python*/site-packages/pydantic_core pydantic_core
+ln -s .venv/lib/python*/site-packages/pydantic_settings pydantic_settings
+ln -s .venv/lib/python*/site-packages/pydantic_extra_types pydantic_extra_types
+
 ${HOME}/.cargo/bin/uv run --no-sync mkdocs build
